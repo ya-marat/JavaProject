535f65f47ac073db2b56c322904988ab7e788230
Frankstella <salataeva.seda@gmail.com>
2026-06-01 21:47:59 +0300

    В классе Sort реализовано доп. задание 1

--- src/main/java/org/example/sort/Sort.java
+++ src/main/java/org/example/sort/Sort.java
@@ -3,6 +3,33 @@ import org.example.model.Car;
 import java.util.Comparator;
 
 public class Sort {
+    public static void addlSort(Car[] arr, Comparator<Car> comparator) {
+        if (arr == null || arr.length <= 1) return;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].getPower() % 2 == 0) {
+                count++;
+            }
+        }
+
+        if (count <= 1) return;
+        Car[] carsToSort = new Car[count];
+        int[] ind = new int[count];
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].getPower() % 2 == 0) {
+                carsToSort[j] = arr[i];
+                ind[j] = i;
+                j++;
+            }
+        }
+
+        quickSort(carsToSort, 0, count - 1, comparator);
+        for (int i = 0; i < count; i++) {
+            arr[ind[i]] = carsToSort[i];
+        }
+    }
+
          public static void quickSort(Car[] arr, int low, int high, Comparator<Car> comparator) {
             if (low < high) {
                 int pi = partition(arr, low, high, comparator);
