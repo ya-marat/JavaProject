ddc921674087a91f66ed5631a696164666183da6
Frankstella <salataeva.seda@gmail.com>
2026-06-03 17:37:19 +0300

    В компаратор по модели доб. мощность и год выпуска

--- src/main/java/org/example/comparator/CarModelComparator.java
+++ src/main/java/org/example/comparator/CarModelComparator.java
@@ -6,7 +6,18 @@ import java.util.Comparator;
 
 public class CarModelComparator implements Comparator<Car> {
     @Override
-    public int compare(Car o1, Car o2) {
-        return o1.getModel().compareTo(o2.getModel());
+    public int compare(Car car1, Car car2) {
+
+        int modelCompare = car1.getModel().compareTo(car2.getModel());
+        if (modelCompare != 0) {
+        return modelCompare;
+        }
+
+        int powerCompare = Integer.compare(car1.getPower(), car2.getPower());
+        if  (powerCompare != 0) {
+            return powerCompare;
+        }
+
+        return Integer.compare(car1.getYear(), car2.getYear());
     }
 }
