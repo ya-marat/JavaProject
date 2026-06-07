af67cacf5e564145779b2ce180544f0cfcaf27f4
Frankstella <salataeva.seda@gmail.com>
2026-06-07 11:27:38 +0300

    в классах Sort, EvenSort массив заменен на коллекцию

--- src/main/java/org/example/sort/EvenSort.java
+++ src/main/java/org/example/sort/EvenSort.java
@@ -1,25 +1,28 @@
 package org.example.sort;
+import org.example.list.CustomList;
 import org.example.model.Car;
 import java.util.Comparator;
 
 public class EvenSort {
 
-    public static void addCarSort(Car[] arr, Comparator<Car> comparator) {
-        if (arr == null || arr.length <= 1) return;
+    public static void addCarSort(CustomList<Car> list, Comparator<Car> comparator) {
+        if (list == null || list.size() <= 1) return;
         int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].getPower() % 2 == 0) {
+        for (int i = 0; i < list.size(); i++) {
+            Car car = list.get(i);
+            if (car != null && car.getPower() % 2 == 0) {
                 count++;
             }
         }
 
         if (count <= 1) return;
-        Car[] carsToSort = new Car[count];
+        CustomList<Car> carsToSort = new CustomList<>();
         int[] ind = new int[count];
         int j = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].getPower() % 2 == 0) {
-                carsToSort[j] = arr[i];
+        for (int i = 0; i < list.size(); i++) {
+            Car car = list.get(i);
+            if (car != null && car.getPower() % 2 == 0) {
+                carsToSort.add(car);
                 ind[j] = i;
                 j++;
             }
@@ -27,7 +30,7 @@ public class EvenSort {
 
         Sort.quickSort(carsToSort, 0, count - 1, comparator);
         for (int i = 0; i < count; i++) {
-            arr[ind[i]] = carsToSort[i];
+            list.set(ind[i], carsToSort.get(i));
         }
     }
 }
--- src/main/java/org/example/sort/Sort.java
+++ src/main/java/org/example/sort/Sort.java
@@ -1,36 +1,37 @@
 package org.example.sort;
+import org.example.list.CustomList;
 import org.example.model.Car;
 import java.util.Comparator;
 
 public class Sort {
 
-    public static void quickSort(Car[] arr, int low, int high, Comparator<Car> comparator) {
+    public static void quickSort(CustomList<Car> list, int low, int high, Comparator<Car> comparator) {
             if (low < high) {
-                int pi = partition(arr, low, high, comparator);
-                quickSort(arr, low, pi - 1, comparator);
-                quickSort(arr, pi + 1, high, comparator);
+                int pi = partition(list, low, high, comparator);
+                quickSort(list, low, pi - 1, comparator);
+                quickSort(list, pi + 1, high, comparator);
             }
         }
 
-        public static int partition(Car[] arr, int low, int high, Comparator<Car> comparator) {
-            Car pivot = arr[high];
+        public static int partition(CustomList<Car> list, int low, int high, Comparator<Car> comparator) {
+            Car pivot = list.get(high);
             int i = (low - 1);
 
             for (int j = low; j < high; j++) {
-                if (comparator.compare(arr[j], pivot) <= 0) {
+                if (comparator.compare(list.get(j), pivot) <= 0) {
                     i++;
-                    swap(arr, i, j);
+                    swap(list, i, j);
                 }
             }
 
-            swap(arr, i + 1, high);
+            swap(list, i + 1, high);
             return i + 1;
         }
 
-        public static void swap(Car[] arr, int i, int j) {
-            Car temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+        public static void swap(CustomList<Car> list, int i, int j) {
+            Car temp = list.get(i);
+            list.set(i, list.get(j));
+            list.set(j, temp);
         }
     }
 
